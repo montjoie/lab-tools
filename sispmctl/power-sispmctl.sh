3522fd22a3ce4121179d06e6f0a195fb681fb902
Corentin Labbe <clabbe.montjoie@gmail.com>
2021-10-04 09:53:56 +0200

    sispmctl: add lock

--- sispmctl/power-sispmctl.sh
+++ sispmctl/power-sispmctl.sh
@@ -2,22 +2,49 @@
 
 if [ $# -le 1 ];then
 	echo "ERROR: need more argument"
+	exit 1
 fi
 
-case $1 in
+lock() {
+	TIMEOUT=0
+	while [ $TIMEOUT -le 40 ]
+	do
+		mkdir /tmp/sispmctl.lock 2>/dev/null
+		if [ $? -eq 0 ];then
+			return 0
+		fi
+		echo "DEBUG: on attend"
+		sleep 30
+		TIMEOUT=$(($TIMEOUT+1))
+	done
+	echo "ERROR: cannot lock (timeout=$TIMEOUT)"
+	return 1
+}
+
+unlock() {
+	rmdir /tmp/sispmctl.lock
+}
+
+case "$1" in
 reset)
-	sispmctl -f $2
+	lock || exit 1
+	sispmctl -f "$2"
 	sleep 3
-	sispmctl -o $2
-	exit $?
+	sispmctl -o "$2"
+	unlock
+	exit 0
 ;;
 off)
-	sispmctl -f $2
-	exit $?
+	lock || exit 1
+	sispmctl -f "$2"
+	unlock
+	exit 0
 ;;
 on)
-	sispmctl -o $2
-	exit $?
+	lock || exit 1
+	sispmctl -o "$2"
+	unlock
+	exit 0
 ;;
 *)
 	echo "ERROR: unknow command: $1"
